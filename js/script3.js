7f7cd68938845178d5d7452c21759bf4aefa4b5d
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2023-06-27 17:37:52 -0300

    d

--- js/script3.js
+++ js/script3.js
@@ -5,51 +5,51 @@ class Forma {
 }
 class FormaCircular extends Forma {
     static pi = 3.14
-    raio
+    #raio
 
     constructor(raio, altura) {
         super(altura)
-        this.raio = raio
+        this.#raio = raio
     }
 
     getRaio() {
-        return this.raio
+        return this.#raio
     }
 
     setRaio(raio) {
-        this.raio = raio
+        this.#raio = raio
     }
 
-    calcularArea(raio) {
-        console.log("Acessou o método calcularArea da classe FormaCircular.")
-        return (FormaCircular.pi * Math.pow(raio, 2)).toFixed(2)
+    calcularArea() {
+        return (FormaCircular.pi * Math.pow(this.#raio, 2)).toFixed(2)
     }
 
-    calcularVolumeCilindro(raio, h) {
+    calcularVolumeCilindro() {
         console.log("Acessou o método calcularVolumeCilindro da classe FormaCircular.")
-        const areaBase =  this.raio.calcularArea(raio)
-        return (areaBase * h).toFixed(2)
+        const areaBase =  this.calcularArea()
+        return (areaBase * this.altura).toFixed(2)
     }
 
-    calcularVolumeCone(raio, h) {
-        console.log("Acessou o método calcularVolumeCone da classe FormaCircular.")
-        const areaBase =  this.raio.calcularArea(raio)
-        return ((areaBase * h) / 3).toFixed(2)
+    calcularVolumeCone() {
+        const areaBase =  this.calcularArea()
+        return ((areaBase *  this.altura) / 3).toFixed(2)
     }
 
-    static calcularVolumeEsfera(raio) {
+    calcularVolumeEsfera() {
         console.log("Acessou o método calcularVolumeEsfera da classe FormaCircular.")
-        const areaBase =  this.raio.calcularArea(raio)
-        return (areaBase * raio * 1.33).toFixed(2)
+        const areaBase =  this.calcularArea()
+        return (areaBase * this.#raio * 1.33).toFixed(2)
     }
 }
 
-const area =  this.raio.calcularArea(raio)
+let circulo = new FormaCircular(3,5)
+
+const area =  circulo.calcularArea()
 console.log(area)
-const volumeCilindro = FormaCircular.calcularVolumeCilindro(raio, altura)
+const volumeCilindro = circulo.calcularVolumeCilindro()
 console.log(volumeCilindro)
-const volumeCone = FormaCircular.calcularVolumeCone(raio, altura)
+const volumeCone = circulo.calcularVolumeCone()
 console.log(volumeCone)
-const volumeEsfera = FormaCircular.calcularVolumeEsfera(raio)
+const volumeEsfera = circulo.calcularVolumeEsfera()
 console.log(volumeEsfera)
 
